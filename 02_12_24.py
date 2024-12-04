7202c4697ceb4caaefb6273e814add9a28d8d770
Matthias Roecklinger <mroecklinger@posteo.at>
2024-12-04 10:07:52 +0100

    number 2.2 is harder than expected

--- 02_12_24.py
+++ 02_12_24.py
@@ -30,6 +30,7 @@ def check_for_limits_decreasing(line):
     safe = True
     return safe
 
+
 def is_increasing_in_order(line):
     exceptions = 0
     index_to_pop = 0
@@ -37,7 +38,7 @@ def is_increasing_in_order(line):
     for i in range(0, len(line) - 1):
         if exceptions > 1:
             return False
-        if line[i+1] > line[i]:
+        if line[i + 1] > line[i]:
             continue
         else:
             index_to_pop = i + 1
@@ -49,9 +50,6 @@ def is_increasing_in_order(line):
     elif exceptions == 1:
         print(exceptions, "how many exceptions")
         return line.pop(index_to_pop)
-    
-    
-
 
 
 def security_dampener_increasing(line):
@@ -87,42 +85,135 @@ def is_safe_without_dampener(line):
         return check_for_limits_increasing(line)
     elif line == _line_decreasing:
         return check_for_limits_decreasing(line)
-
-
-def is_safe_with_dampener(line):
-    if (security_dampener_increasing(line) is False) & (
-        security_dampener_decreasing(line) is False
-    ):
-        return False
-    elif (isinstance(security_dampener_increasing(line), list)) & (
-        security_dampener_decreasing(line) is False
-    ):
-        return check_for_limits_increasing(line)
-    elif (security_dampener_increasing(line) is False) & (
-        isinstance(security_dampener_decreasing(line), list)
-    ):
-        return check_for_limits_decreasing(line)
     else:
-        print("WTF")
+        return False
+    
+def increasing_with_damper(line):
+    _index_of_anomalies = []
+    for i in range(0, len(line) - 1):
+        print(line[i+1], line[i])
+        if line[i+1] > line[i]:
+            continue
+        else:
+            print("ARE YOU EVEN APPENDING", i)
+            _index_of_anomalies.append(i)
+    print("this is list of anomalies:", _index_of_anomalies, len(_index_of_anomalies))
+    if len(_index_of_anomalies) == 1:
+        line.pop(_index_of_anomalies[0])
+        return line
+    else:
+        return False
+    
+def decreasing_with_damper(line):
+    _index_of_anomalies = []
+    for i in range(0, len(line) -1):
+        if line[i] > line[i+1]:
+            continue
+        else:
+            _index_of_anomalies.append(i+1)
+    if len(_index_of_anomalies) == 1:
+        line.pop(_index_of_anomalies[0])
+        return line
+    else:
+        return False
+    
+def is_increasing(line):
+    """compares first and last item to check if it is increasing"""
+    if line[0] > line[-1]:
+        return False
+    elif line[0] < line[-1]:
+        return True
+    else:
+        print("THEY ARE THE SAME PICTURE, ANOMALY DETECTED")
+
+    
 
 
-def main():
-    with open("02_12_demo_input.txt", "r") as f:
+def main_1():
+    with open("input_02_12.txt", "r") as f:
         data = f.readlines()
     print(len(data))
     number_of_safe_rows = 0
-    number_of_safe_rows_with_dampener = 0
     for line in data:
         line_clean = clean_line(line)
         if is_safe_without_dampener(line_clean):
             number_of_safe_rows += 1
-        print(line)
-        if is_safe_with_dampener(line_clean):
-            number_of_safe_rows_with_dampener += 1
-        print(is_increasing_in_order(line))
     print("we have", number_of_safe_rows, "safe rows without dampener")
-    print("we have", number_of_safe_rows_with_dampener, "safe rows with dampener")
+
+
+## meta algo
+# go through line by line
+# check if it matches increasing or decreasing sorted list
+## 
+def main_2():
+    with open("02_12_demo_input.txt", "r") as f:
+        data = f.readlines()
+    number_of_safe_rows = 0
+    for line in data:
+        line_cleaned = clean_line(line)
+        print(line_cleaned)
+        if is_safe_without_dampener(line_cleaned):
+            number_of_safe_rows += 1
+        elif not decreasing_with_damper(line_cleaned):
+            line_popped = increasing_with_damper(line_cleaned)
+            print("this is line_popped", line_popped)
+            if line_popped == sorted(line_popped):
+                number_of_safe_rows += 1
+            else:
+                continue
+        elif not increasing_with_damper(line_cleaned):
+            line_popped = decreasing_with_damper(line_cleaned)
+            if line_popped == sorted(line_popped, reverse=True):
+                number_of_safe_rows += 1
+            else:
+                continue
+        else:
+            print("SOMETHIN SLIPPED THROUGH")
+
+    print("we have following number of accepted rows:", number_of_safe_rows)
+    
+
+
+def main_3():
+    with open("input_02_12.txt", "r") as f:
+        data = f.readlines()
+    number_of_safe_rows = 0
+    for line in data:
+        line_cleaned = clean_line(line)
+        if is_increasing(line_cleaned):
+            print(line_cleaned, increasing_with_damper(line_cleaned))
+            ### can be already fine instruction
+            if (line_cleaned == sorted(line_cleaned)) & check_for_limits_increasing(line_cleaned):
+                number_of_safe_rows += 1
+            elif (line_cleaned == sorted(line_cleaned)) and not check_for_limits_increasing(line_cleaned):
+                continue
+            elif not increasing_with_damper(line_cleaned):
+                print("not going in here?")
+                continue
+            else:
+                reduced_line = increasing_with_damper(line_cleaned)
+                print("THIS IS THE LINE" ,reduced_line)
+                if check_for_limits_increasing(reduced_line):
+                    number_of_safe_rows += 1
+                else:
+                    continue
+        if not is_increasing(line_cleaned):
+            print(line_cleaned, decreasing_with_damper(line_cleaned))
+            if (line_cleaned == sorted(line_cleaned, reverse=True)) & check_for_limits_decreasing(line_cleaned):
+                number_of_safe_rows += 1
+            elif (line_cleaned == sorted(line_cleaned, reverse=True)) and not check_for_limits_decreasing(line_cleaned):
+                continue
+            elif not decreasing_with_damper(line_cleaned):
+                continue
+            else:
+                reduced_line = decreasing_with_damper(line_cleaned)
+                if check_for_limits_decreasing(reduced_line):
+                    number_of_safe_rows += 1
+                else:
+                    continue
+    print("we have following number of safe rows:", number_of_safe_rows)
+                
 
 
 if __name__ == "__main__":
-    main()
+    main_3()
--- 03_12_24.py
+++ 03_12_24.py
@@ -1,9 +1,7 @@
 import re
 
 REGEX_MULTIPLICATION = r"mul\((?P<first>\d{,3}),(?P<second>\d{,3})\)"
-
-with open("input_03_12.txt", "r") as f:
-    data = f.readline()
+REGEX_FIND = r"mul\(\d{,3}\,\d{,3}\)"
 
 
 def clean_and_multiply(item):
@@ -12,10 +10,42 @@ def clean_and_multiply(item):
     return mult
 
 
-_ = re.findall(r"mul\(\d{,3}\,\d{,3}\)", data)
-print(_)
-sum_of_mult = 0
-for item in _:
-    sum_of_mult += clean_and_multiply(item)
+def main_1():
+    with open("input_03_12.txt", "r") as f:
+        data = f.readline()
+    _ = re.findall(REGEX_FIND, data)
+    sum_of_mult = 0
+    for item in _:
+        sum_of_mult += clean_and_multiply(item)
+    print("we have a sum of:", sum_of_mult)
 
-print("we have a sum of:", sum_of_mult)
+
+def main_2():
+    with open("input_03_12.txt", "r") as f:
+        data = f.readline()
+    data_split = data.split("don't()")
+    print("we have following number of split items:", len(data_split))
+    # because we start with enabled
+    sum_of_activated_mult = 0
+    first_part = data_split[0]
+    match_in_first_part = re.findall(REGEX_FIND, first_part)
+    for item in match_in_first_part:
+        sum_of_activated_mult += clean_and_multiply(item)
+    for line in data_split[1:]:
+        # split for activation
+        line_split = line.split("do()")
+        # double de-activation
+        if len(line_split) == 1:
+            print("no double activation")
+            continue
+        ## we need another loop to include all dos
+        # [0] is don't() --> [1:]
+        for _do in line_split[1:]:
+            match_activated = re.findall(REGEX_FIND, _do)
+            for item in match_activated:
+                sum_of_activated_mult += clean_and_multiply(item)
+    print("we have following sum of activated instructions:", sum_of_activated_mult)
+
+
+if __name__ == "__main__":
+    main_2()
